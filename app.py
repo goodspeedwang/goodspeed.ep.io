b10d8dcfe20b53e7bcd738f70576c30d45ad6f80
goodspeed <goodspeedwang@gmail.com>
2011-11-16 09:51:31 +0800

    favicon.ico

--- app.py
+++ app.py
@@ -1,9 +1,21 @@
-from flask import Flask
+from flask import Flask,send_from_directory
+import os
+
 app = Flask(__name__)
+
 
 @app.route('/')
 def index():
     return 'Hello from Flask!\n'
+
+@app.route('/proxy')
+def proxy():
+    return "Proxy is here."
+
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
 if __name__ == '__main__':
     app.run(debug=True)
